78b48e5b01ac63403a6214e7d6e6cd68aa274f8f
Alexander Gnatenko <agnatenko@gmail.com>
2020-12-25 04:02:27 +0300

    Редактирование файла Main

--- src/main/java/ru/idcore/Main.java
+++ src/main/java/ru/idcore/Main.java
@@ -1,13 +1,21 @@
 package ru.idcore;
 
+import ru.idcore.service.CSVFile;
+import ru.idcore.service.Delimiter;
+
+import java.io.IOException;
+
 /**
  * Parser CSV_JSON
  *
  */
 public class Main
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        //System.out.println( "Hello World!" );
+        CSVFile.about();
+
+        String csvFile = "test.csv";
+        CSVFile.createCSVFile("1;John;Smith;USA;25", csvFile, Delimiter.DELIMITER_SEMICOLON, true );
     }
 }
