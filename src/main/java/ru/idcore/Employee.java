6c785b83f92f0537ec7480d6e4758ebf8cc195d8
Alexander Gnatenko <agnatenko@gmail.com>
2020-12-28 01:54:50 +0300

    Редактирование файла Main

--- src/main/java/ru/idcore/Employee.java
+++ src/main/java/ru/idcore/Employee.java
@@ -1,91 +1,17 @@
 package ru.idcore;
 
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvBindByPosition;
+
 import java.util.Objects;
 
 public class Employee {
-    private long id;
+    @CsvBindByName
     private String firstName;
-    private String secondName;
-    private String country;
-    private int age;
 
-    Employee() {
+    @CsvBindByName
+    private String lastName;
 
-    }
-
-    Employee(long id, String firstName, String secondName, String country, int age) {
-        this.id = id;
-        this.firstName = firstName;
-        this.secondName = secondName;
-        this.country = country;
-        this.age = age;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getSecondName() {
-        return secondName;
-    }
-
-    public void setSecondName(String secondName) {
-        this.secondName = secondName;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", secondName='" + secondName + '\'' +
-                ", country='" + country + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Employee)) return false;
-        Employee employee = (Employee) o;
-        return getId() == employee.getId() &&
-                getAge() == employee.getAge() &&
-                getFirstName().equals(employee.getFirstName()) &&
-                getSecondName().equals(employee.getSecondName()) &&
-                getCountry().equals(employee.getCountry());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getFirstName(), getSecondName(), getCountry(), getAge());
-    }
+    @CsvBindByName
+    private int visitsToWebsite;
 }
--- src/main/java/ru/idcore/Main.java
+++ src/main/java/ru/idcore/Main.java
@@ -1,21 +1,22 @@
 package ru.idcore;
 
-import ru.idcore.service.CSVFile;
-import ru.idcore.service.Delimiter;
 
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 /**
  * Parser CSV_JSON
- *
  */
-public class Main
-{
-    public static void main( String[] args ) throws IOException {
-        //System.out.println( "Hello World!" );
-        CSVFile.about();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        List<Employee> beans = new CsvToBeanBuilder<Employee>(new FileReader("test.csv"))
+                .withType(Employee.class).withSeparator(';').build().parse();
 
-        String csvFile = "test.csv";
-        CSVFile.createCSVFile("1,John,Smith,USA,25", csvFile, Delimiter.DELIMITER_COMMA, true );
+        beans.forEach(System.out::println);
     }
+
+
 }
